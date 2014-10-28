78c7ce20800af98626c0f9afdbbb2f8499b4a9bc
leizhou <leizhou@oupeng.com>
2014-10-28 13:55:15 +0800

    change

--- Public/js/moveloc.js
+++ Public/js/moveloc.js
@@ -1,27 +1,20 @@
 var flag;
 function addloc(locs,mylocs)
 {
- if (mylocs.length>17) alert('您选择的项目超过18个了！请删除部分项目再选！');
+ if (mylocs.options.length>5) alert('您选择的项目超过18个了！请删除部分项目再选！');
  else
  {
- 	for(var x=0;x<locs.length;x++)
-	{
- 		var opt = locs.options[x];
- 		if (opt.selected)
- 		{
- 			flag = true;
- 			for (var y=0;y<mylocs.length;y++)
- 			{
- 				var myopt = mylocs.options[y];
- 				if (myopt.value == opt.value) flag = false;
- 			}
- 			if(flag)
-				mylocs.options[mylocs.options.length] = new Option(opt.text, opt.value, 0, 0);
-		 }
-	}
+ 	flag = true;
+ 	for (var y=0;y<mylocs.length;y++)
+ 	{
+ 		var myopt = mylocs.options[y];
+ 		if (myopt.value == locs) flag = false;
+ 	}
+ 	if(flag)
+ 		mylocs.options[mylocs.options.length] = new Option(locs, locs, 0, 0);
  }
 }
-function delloc(locs,mylocs)
+function delloc(mylocs)
 {
  for(var x=mylocs.length-1;x>=0;x--)
  {
